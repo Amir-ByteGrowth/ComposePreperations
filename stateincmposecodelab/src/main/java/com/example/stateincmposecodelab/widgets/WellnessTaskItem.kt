905eb99528c95d8d3f0576f377300421931e634d
Amir javeed <amir@vaultspay.ae>
2023-11-06 10:25:12 +0400

    close and check uncheck functionality added

--- stateincmposecodelab/src/main/java/com/example/stateincmposecodelab/widgets/WellnessTaskItem.kt
+++ stateincmposecodelab/src/main/java/com/example/stateincmposecodelab/widgets/WellnessTaskItem.kt
@@ -13,13 +13,14 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.stateincmposecodelab.data.WellnessItem
 import com.example.stateincmposecodelab.data.getWellnessList
@@ -28,28 +29,40 @@ import com.example.stateincmposecodelab.data.getWellnessList
 @Composable
 fun WellnessTasksList(
     modifier: Modifier = Modifier,
-    list: List<WellnessItem> = remember { getWellnessList() }
-) {
 
 
+    ) {
 
+//    val list = remember { getWellnessList().toMutableStateList() }
+    val list = remember {
+
+        mutableStateListOf<WellnessItem>().apply { addAll(getWellnessList()) }
+
+    }
     LazyColumn(
         modifier = modifier,
-       state = rememberLazyListState()
+        state = rememberLazyListState()
     ) {
-        items(list) { item -> WellnessTaskItem(taskName = item.name) }
+        items(list, key = {task -> task.id}) { item ->
+            WellnessTaskItem(taskName = item.name,
+                {  list.remove(item) })
+        }
     }
 }
 
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+fun WellnessTaskItem(
+    taskName: String,
+    onCloseTask: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     var checkedState by rememberSaveable { mutableStateOf(false) }
 
     WellnessTaskItem(
         taskName = taskName,
         checked = checkedState,
         onCheckChanged = { newValue -> checkedState = newValue },
-        onClose = {}, // we will implement this later!
+        onClose =   onCloseTask , // we will implement this later!
         modifier = modifier,
     )
 }
@@ -79,8 +92,8 @@ fun WellnessTaskItem(
 }
 
 
-@Preview
-@Composable
-fun PreviewItem() {
-    WellnessTaskItem("FirstItem", false, { checked -> }, {})
-}
+//@Preview
+//@Composable
+//fun PreviewItem() {
+//    WellnessTaskItem("FirstItem", false, { checked -> }, {})
+//}
